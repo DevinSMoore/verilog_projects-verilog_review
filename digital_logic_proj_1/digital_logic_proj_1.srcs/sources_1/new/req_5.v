3df56d3f1c7a5dc0237a6493d854541948c584f6
DevinSMoore <moore.ds04@gmail.com>
2022-06-05 09:28:15 -0700

    added req_5 constraints file to vscode workspace

--- digital_logic_proj_1/digital_logic_proj_1.srcs/sources_1/new/req_5.v
+++ digital_logic_proj_1/digital_logic_proj_1.srcs/sources_1/new/req_5.v
@@ -21,6 +21,45 @@
 
 
 module req_5(
+    input [11:8] i_sw,
+    input [3:0] i_sw,
+    output [3:0] o_seg_an,
+    output [6:0] o_seg_cat
+);
 
-    );
+case (i_sw)
+    4'b0000 : begin
+        assign o_anode_sel[]
+    end
+    4'b0001 : begin
+
+    end
+    4'b0010 : begin
+    
+    end
+    4'b0011 : begin
+    
+    end
+    4'b0100 : begin
+    
+    end
+    4'b0101 : begin
+
+    end
+    4'b0110 : begin
+
+    end
+    4'b0111 : begin
+    
+    end
+    4'b1000 : begin
+
+    end
+    4'b1001 : begin
+
+    end
+    default: 
+endcase
+
+
 endmodule
